ef5ced9571b2946ab710c77e82edc63216f1887c
Robert James <rjames@e-tcc.com>
2015-11-10 10:42:18 -0500

    Add simple express server, seding harded

---
+++ server/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.get('/notes', function(req, res) {
+  res.json([
+    {
+      title:  'Hardcoded note',
+      body_html:  'This is the body of my hardcoded note.'
+    },
+    {
+      title: 'Another one',
+      body_html:  'This is the cody of my hardcoded note 2'
+    }
+  ]);
+});
+
+app.listen(3000, function() {
+  console.log('Listening on http://localhost:3000');
+});
