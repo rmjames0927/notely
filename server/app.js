996868d6ccdda0a0a16d20416de0e4ed2589bf92
Robert James <rjames@e-tcc.com>
2015-11-10 15:01:06 -0500

    reading from the database

--- server/app.js
+++ server/app.js
@@ -1,22 +1,31 @@
 var express = require('express');
 var app = express();
 
+//Database
+var db = require('mongoose');
+//Database Connection String
+db.connect('mongodb://mongo:newpass#1@ds031912.mongolab.com:31912/notelydb');
+//Database Schema Like Create Script
+var NoteSchema = db.Schema({
+  title: String,
+  body_html: String,
+  body_text: String,
+  updated_at:  { type: Date, default: Date.now }
+});
+
+//Database Model [first what mongoose knows it at] [second is our schema created previous]
+var Note = db.model('note', NoteSchema);
+
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
+//List all notes
 app.get('/notes', function(req, res) {
-  res.json([
-    {
-      title:  'Hardcoded note',
-      body_html:  'This is the body of my hardcoded note.'
-    },
-    {
-      title: 'Another one',
-      body_html:  'This is the cody of my hardcoded note 2'
-    }
-  ]);
+  Note.find().then(function(notes) {
+    res.json(notes);
+  });
 });
 
 app.listen(3000, function() {
