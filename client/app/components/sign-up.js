b4c5c464a3c315c773473bb176a4b7939ae35283
Robert James <rjames@e-tcc.com>
2015-11-12 11:30:53 -0500

    sign-up controller and form

--- client/app/components/sign-up.js
+++ client/app/components/sign-up.js
@@ -1,6 +1,21 @@
 angular.module('notely')
-.directive('signUp', function() {
+.directive('signUp', () => {
+
+  class SignUpController {
+    constructor() {
+      this.user = {};
+    }
+    submit() {
+      alert(this.user.name);
+      console.log(this.user);
+    }
+  }
+
   return {
+    scope: {},
+    controller: SignUpController,
+    controllerAs: 'ctrl',
+    bindToController: true,
     templateUrl: '/components/sign-up.html'
   };
 });
--- client/app/content/bundle.js
+++ client/app/content/bundle.js
@@ -14,11 +14,100 @@
 })();
 'use strict';
 
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
 angular.module('notely').directive('signUp', function () {
+  var SignUpController = (function () {
+    function SignUpController() {
+      _classCallCheck(this, SignUpController);
+
+      this.user = {};
+    }
+
+    _createClass(SignUpController, [{
+      key: 'submit',
+      value: function submit() {
+        alert(this.user.name);
+        console.log(this.user);
+      }
+    }]);
+
+    return SignUpController;
+  })();
+
   return {
+    scope: {},
+    controller: SignUpController,
+    controllerAs: 'ctrl',
+    bindToController: true,
     templateUrl: '/components/sign-up.html'
   };
 });
+'use strict';
+
+(function () {
+
+  angular.module('notely.notes', ['ui.router', 'textAngular']).config(notesConfig);
+  // This is my comment
+  notesConfig['$inject'] = ['$stateProvider'];
+  function notesConfig($stateProvider) {
+    $stateProvider.state('notes', {
+      url: '/notes',
+      resolve: {
+        notesLoaded: ['NotesService', function (NotesService) {
+          return NotesService.fetch();
+        }]
+      },
+      templateUrl: '/notes/notes.html',
+      controller: NotesController
+    }).state('notes.form', {
+      url: '/:noteId',
+      templateUrl: '/notes/notes-form.html',
+      controller: NotesFormController
+    });
+  }
+
+  NotesController.$inject = ['$state', '$scope', 'NotesService'];
+  function NotesController($state, $scope, NotesService) {
+    $scope.notes = {};
+    $scope.notes = NotesService.get();
+  }
+
+  NotesFormController.$inject = ['$state', '$scope', 'NotesService'];
+  function NotesFormController($state, $scope, NotesService) {
+    $scope.note = NotesService.findById($state.params.noteId);
+
+    $scope.save = function () {
+      // Decide whether to call create or update...
+      if ($scope.note._id) {
+        NotesService.update($scope.note).then(function (response) {
+          $scope.note = angular.copy(response.data.note);
+        });
+      } else {
+        NotesService.create($scope.note).then(function (response) {
+          $state.go('notes.form', { noteId: response.data.note._id });
+        }).then(function () {
+          $scope.latestResult = NotesService.getMessage();
+        });
+      }
+    };
+
+    $scope['delete'] = function () {
+      NotesService['delete']($scope.note).then(function (repsonse) {
+        $state.go('notes.form', { noteId: undefined });
+      });
+    };
+
+    $scope.buttonText = function () {
+      if ($scope.note._id) {
+        return 'Save Changes';
+      }
+      return 'Save';
+    };
+  }
+})();
 'use strict';
 
 angular.module('notely').service('NotesService', NotesService);
@@ -118,69 +207,6 @@ function NotesService($http, API_BASE) {
     }
   };
 }
-'use strict';
-
-(function () {
-
-  angular.module('notely.notes', ['ui.router', 'textAngular']).config(notesConfig);
-  // This is my comment
-  notesConfig['$inject'] = ['$stateProvider'];
-  function notesConfig($stateProvider) {
-    $stateProvider.state('notes', {
-      url: '/notes',
-      resolve: {
-        notesLoaded: ['NotesService', function (NotesService) {
-          return NotesService.fetch();
-        }]
-      },
-      templateUrl: '/notes/notes.html',
-      controller: NotesController
-    }).state('notes.form', {
-      url: '/:noteId',
-      templateUrl: '/notes/notes-form.html',
-      controller: NotesFormController
-    });
-  }
-
-  NotesController.$inject = ['$state', '$scope', 'NotesService'];
-  function NotesController($state, $scope, NotesService) {
-    $scope.notes = {};
-    $scope.notes = NotesService.get();
-  }
-
-  NotesFormController.$inject = ['$state', '$scope', 'NotesService'];
-  function NotesFormController($state, $scope, NotesService) {
-    $scope.note = NotesService.findById($state.params.noteId);
-
-    $scope.save = function () {
-      // Decide whether to call create or update...
-      if ($scope.note._id) {
-        NotesService.update($scope.note).then(function (response) {
-          $scope.note = angular.copy(response.data.note);
-        });
-      } else {
-        NotesService.create($scope.note).then(function (response) {
-          $state.go('notes.form', { noteId: response.data.note._id });
-        }).then(function () {
-          $scope.latestResult = NotesService.getMessage();
-        });
-      }
-    };
-
-    $scope['delete'] = function () {
-      NotesService['delete']($scope.note).then(function (repsonse) {
-        $state.go('notes.form', { noteId: undefined });
-      });
-    };
-
-    $scope.buttonText = function () {
-      if ($scope.note._id) {
-        return 'Save Changes';
-      }
-      return 'Save';
-    };
-  }
-})();
 'use strict';
 
 {
